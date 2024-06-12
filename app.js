b939367678769cad959955f04e129013dd5138b9
Joshua Adrian <joshua@joshuaadrian.com>
2024-06-12 17:34:01 -0500

    updates

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ let pals = {
 		'description' : 'I’m a very important pollinator in desert and tropical climates.  I love the insects and the nectar that I find in flowers, and I’m wild about banana trees.  In addition to banana trees, mangoes, guavas and agave also depend on me for pollination.  Sometimes I even stumble upon and hummingbird feeder and drain it before the birds wake up.  You know what they say: the early bat gets the sweets!',
 		'image' : 'bat.png'
 	},
-		'bee' : {
+	'bee' : {
 		'title' : 'You most identify with a Bee',
 		'description' : 'Scientists would say that bees are flying insects that collect pollen and nectar, but I’m a lot more than that.  For instance, did you know I have four wings?  The front and back wings hook together when I fly for maximum lift.  Now that’s efficient!  I’m fast, too, flying up to 15 mph.  Don’t try to outrun me!  Do you want to know a secret?  Everyone loves me for my honey, but only about 100 out of 20,000 species of bees make honey at all.  We’re the world’s most important pollinators, so look past the honey and love me for bee-ing me!',
 		'image' : 'bee.png'
@@ -59,7 +59,7 @@ let pals = {
 	},
 	'fly' : {
 		'title' : 'You most identify with a Fly',
-		'description' : 'Bees may get most of the credit in the pollinator world, but we flies are just as important.  In fact, flies may out-pollinate bees in the early spring because we’re active at lower temperatures.  We zip from flower to flower and transport pollen on our bodies as we go.  The worse a flower smells to people, the more delicious it is to me.  One of my favorites meals comes from a skunky trillium that the humans call “Stinking-Benjamin.”',
+		'description' : 'Bees may get most of the credit in the pollinator world, but we flies are just as important.  In fact, flies may out-pollinate bees in the early spring because we’re active at lower temperatures.  We zip from flower to flower and transport pollen on our bodies as we go.  The worse a flower smells to people, the more delicious it is to me.  One of my favorite meals comes from a skunky trillium that humans call Stinking-Benjamin.',
 		'image' : 'fly.png'
 	},
 	'beetle' : {
@@ -87,7 +87,7 @@ if (next) {
 		tabs.style.transform = 'translateX('+percent+'%)';
 		progress[current-1].classList.add('answered');
 
-		if ( next.dataset.current >= 4 ) {
+		if ( next.dataset.current >= 8 ) {
 			next.style.display = 'none';
 			submit.style.display = 'block';
 			gift.classList.add('is-showing');
@@ -103,6 +103,8 @@ let whichPal = function() {
 
 	let data = new FormData(form);
 	let arr = [];
+
+	console.log(data);
 
 	data.forEach(function(value) {
 	  arr.push(value);
